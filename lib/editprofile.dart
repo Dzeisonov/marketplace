91b69b06c758f8d8312cf6f7b1715a84628ed9a6
Marchella Angelina <marchellangelina@gmail.com>
2023-09-27 10:29:51 +0700

    Menambahkan fitur pilih avatar di halaman edit profil dan memunculkan toast data berhasil disimpan

--- lib/editprofile.dart
+++ lib/editprofile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-
 class EditProfileScreen extends StatefulWidget {
   EditProfileScreen({Key? key}) : super(key: key);
 
@@ -10,6 +9,8 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
+  String selectedAvatar = "lib/images/avatar1.jpg"; // avatar1 dijadikan sebagai foto profil default
+
   void _showToast(String message) {
     Fluttertoast.showToast(
       msg: message,
@@ -18,6 +19,13 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       backgroundColor: Colors.black,
       textColor: Colors.white,
     );
+  }
+
+  void _selectAvatar(String path) {
+    setState(() {
+      selectedAvatar = path;
+    });
+    Navigator.pop(context);
   }
 
   @override
@@ -34,16 +42,17 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              SizedBox(height: 20), 
               Center(
-                child: _buildProfileIcon("lib/images/profile.png"),
+                child: _buildProfileIcon(selectedAvatar),
               ),
               SizedBox(height: 20),
-              _buildEditProfileItem("Full Name"),
-              _buildEditProfileItem("Username"),
-              _buildEditProfileItem("Email"),
-              _buildEditProfileItem("Phone Number"),
-              _buildEditProfileItem("Address"),
-              _buildEditProfileItem("Bio"),
+              _buildEditProfileItem("Full Name", "Enter your full name..."),
+              _buildEditProfileItem("Username", "Enter your username..."),
+              _buildEditProfileItem("Email", "Enter your email..."),
+              _buildEditProfileItem("Phone Number", "Enter your phone number..."),
+              _buildEditProfileItem("Address", "Enter your address..."),
+              _buildEditProfileItem("Bio", "Enter your bio..."),
               SizedBox(height: 20),
               _buildSaveButton(),
             ],
@@ -53,14 +62,15 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     );
   }
 
-  Widget _buildEditProfileItem(String label) {
+  Widget _buildEditProfileItem(String label, String hint) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         TextFormField(
           style: TextStyle(fontSize: 18),
           decoration: InputDecoration(
-            labelText: label, 
+            labelText: label,
+            hintText: hint,
             border: OutlineInputBorder(),
           ),
         ),
@@ -70,75 +80,92 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   }
 
   Widget _buildProfileIcon(String path) {
-  return GestureDetector(
-    onTap: () {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Profile Photo"),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ListTile(
-                  leading: Icon(Icons.camera),
-                  title: Text("Take a Photo"),
-                  onTap: () {
-                    Navigator.pop(context); 
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Choose an Avatar"),
+              content: Container(
+                width: double.maxFinite,
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemCount: 6, 
+                  itemBuilder: (context, index) {
+                    final avatarPath = "lib/images/avatar${index + 1}.jpg";
+                    final isSelected = avatarPath == selectedAvatar;
+
+                    return GestureDetector(
+                      onTap: () {
+                        _selectAvatar(avatarPath);
+                      },
+                      child: Container(
+                        margin: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: isSelected ? Colors.green : Colors.transparent, // Menampilkan border hijau jika dipilih
+                            width: 4.0,
+                          ),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundImage: AssetImage(avatarPath),
+                            ),
+                            if (isSelected)
+                              Icon(
+                                Icons.check,
+                                color: Colors.green,
+                                size: 40,
+                              ),
+                          ],
+                        ),
+                      ),
+                    );
                   },
                 ),
-                ListTile(
-                  leading: Icon(Icons.photo),
-                  title: Text("Choose from Gallery"),
-                  onTap: () {
-                    Navigator.pop(context); 
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
                   },
+                  child: Text("Cancel"),
                 ),
               ],
-            ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context); 
-                },
-                child: Text("Cancel"),
-              ),
-            ],
-          );
-        },
-      );
-    },
-    child: Container(
-      margin: EdgeInsets.all(10.0),
-      padding: EdgeInsets.all(4.0),
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: const Color.fromARGB(255, 204, 204, 204),
-          width: 4.0,
-        ),
-      ),
-      child: CircleAvatar(
-        radius: 60,
-        backgroundImage: AssetImage(path),
-        child: Center(
-          // Icon + untuk ubah foto profil (ambil foto baru atau pilih dari galeri)
-          child: Icon(
-            Icons.add,
-            size: 60, 
-            color: Colors.grey, 
+            );
+          },
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.all(4.0),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: const Color.fromARGB(255, 196, 196, 196),
+            width: 4.0,
           ),
         ),
+        child: CircleAvatar(
+          radius: 60,
+          backgroundImage: AssetImage(path),
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
   Widget _buildSaveButton() {
     return Center(
       child: ElevatedButton(
         onPressed: () {
-          //_showToast("Data saved successfully");
+          _showToast("Data saved successfully");
         },
         style: ElevatedButton.styleFrom(
           primary: Colors.black,
