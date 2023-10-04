265c6943d2ed896425f24ddb3b0c906837fbfbb7
Kevin Jonathan JM <kevinlovindo.kl@gmail.com>
2023-10-04 19:13:01 +0700

    Finishing Sign Up Page

--- lib/routs.dart
+++ lib/routs.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/widgets.dart';
 import 'package:marketplace/navbar.dart';
+import 'package:marketplace/screens/complete_profile/cpscreen.dart';
 import 'package:marketplace/screens/forgotpw/forgotpw.dart';
 import 'package:marketplace/screens/login/login.dart';
 import 'package:marketplace/screens/loginsuccess/loginscs.dart';
 import 'package:marketplace/screens/paysuccess/payscs.dart';
 import 'package:marketplace/screens/signup/signup.dart';
+import 'package:marketplace/screens/signupsuccess/signupscs.dart';
 import 'package:marketplace/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
@@ -15,4 +17,6 @@ final Map<String, WidgetBuilder> routes = {
   PayScsScreen.routeName: (context) => PayScsScreen(),
   SignUpScreen.routeName: (context) => SignUpScreen(),
   NavBar.routeName: (context) => NavBar(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
+  SignUpScsScreen.routeName: (context) => SignUpScsScreen(),
 };
